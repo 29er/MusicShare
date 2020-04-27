47ca332a1c8a38df965a8cd5e7e0550942397c58
Doug Wood <wood.doug@gmail.com>
2020-04-27 09:20:17 -0600

    add music key

--- src/components/tab-navigation/add-media.js
+++ src/components/tab-navigation/add-media.js
@@ -1,10 +1,36 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import {
+  Image,
+  Linking,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+} from 'react-native';
 
 function AddMediaTab() {
+  const imageUrl =
+    'https://i.scdn.co/image/ab67616d0000b2738233b7f811afd19bbbcd3743';
+
+  const albumUrl = 'https://open.spotify.com/album/5idokHIibbDsmLLHyvwwQp';
+
+  const onOpenPress = () => {
+    Linking.canOpenURL(albumUrl).then(supported => {
+      if (supported) {
+        Linking.openURL(albumUrl);
+      }
+    });
+  };
+
   return (
     <View style={styles.container}>
-      <Text>AddMediaTab</Text>
+      <TouchableOpacity style={styles.imageContainer} onPress={onOpenPress}>
+        <Image
+          source={{
+            uri: imageUrl,
+          }}
+          style={styles.image}
+        />
+      </TouchableOpacity>
     </View>
   );
 }
@@ -16,5 +42,10 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  imageContainer: {},
+  image: {
+    width: 200,
+    height: 200,
   },
 });
